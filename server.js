ea2d80c2639e8bf43ff619c557cb22a047ccd379
Latif Doğan Topluoğlu <latiftopluoglu@gmail.com>
2022-04-06 15:43:49 +0300

    MongoDB connection completed

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const graphqlHTTP = require("express-graphql").graphqlHTTP;
 const schema = require("./schema/schema");
 const app = express();
@@ -14,6 +14,12 @@ app.use(
 );
 
 const PORT = process.env.PORT || 3000;
+mongoose
+  .connect(
+    "mongodb+srv://dgn123:dgn123@cluster0.a0t1y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("MongoDB Connection Successfly"))
+  .catch((err) => console.error(err));
 app.listen(PORT, () => {
   console.log("Connection Success");
 });
